048e855f806b8fcffabf879d0d8d1975b1f3d879
aziz-arfaoui <aziz99arfaoui@gmail.com>
2025-11-05 11:42:26 +0100

    bootstrap

---
+++ CoffeeManager/CoffeeManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeManagerApp.swift
+//  CoffeeManager
+//
+//  Created by Aziz Arfaoui on 5/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
